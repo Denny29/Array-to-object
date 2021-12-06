8a5ce221639e8e06d902f7bece69cd52ea1f87cb
Denny29 <43240668+Denny29@users.noreply.github.com>
2021-12-06 17:40:50 -0500

    Done (hopefully)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "./styles.css";
 
+function printNamesarr(name) {
+  return (
+    <ul>
+      <li>{name[0]}</li>
+      <li>{name[1]}</li>
+    </ul>
+  );
+}
+function printNamesobj(name) {
+  return (
+    <ul>
+      <li>{name.firstName}</li>
+      <li>{name.lastName}</li>
+    </ul>
+  );
+}
+
 export default function App() {
   let people = [
     ["Ariana", "Grande"],
     ["Bruno", "Mars"],
     ["Kendrick", "Lamar"],
     ["Doja", "Cat"]
+  ];
+
+  let people2 = [
+    { firstName: "Ariana", lastName: "Grande" },
+    { firstName: "Bruno", lastName: "Mars" },
+    { firstName: "Kendrick", lastName: "Lamar" },
+    { firstName: "Doja", lastName: "Cat" }
   ];
 
   // TODO 1: print out bruno mars's name by referencing people
@@ -16,6 +40,8 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <div>{people2.map(printNamesobj)}</div>
+      <div>{people.map(printNamesarr)}</div>
     </div>
   );
 }
